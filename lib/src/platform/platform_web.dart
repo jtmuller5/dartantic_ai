a23f1f4bf7689dc1b637f3631289594e588a95e3
Chris Sells <csells@sellsbrothers.com>
2025-06-17 08:53:03 -0700

    better web environment variable access error message

--- lib/src/platform/platform_web.dart
+++ lib/src/platform/platform_web.dart
@@ -3,5 +3,8 @@
 /// Throws an exception because environment variables are not supported on the
 /// web.
 String getEnv(String key) {
-  throw Exception('Environment variable $key is not supported on the web');
+  throw Exception(
+    'Environment variable $key is not supported on the web. '
+    'Set Agent.apiKey or Provider.apiKey explicitly.',
+  );
 }
